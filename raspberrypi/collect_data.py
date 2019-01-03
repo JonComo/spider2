195c62427d5be87df94f68f5341e404be536f815
joncomo <jon@underground.net>
2019-01-03 15:44:50 -0800

    better collect

--- raspberrypi/collect_data.py
+++ raspberrypi/collect_data.py
@@ -12,15 +12,15 @@ for i in range(8):
 
 def move_to(state):
     for i in range(8):
-        servo.setTarget(i, int(state[i]*2000 + 6000))
-    
-    avg_iters = 10
+        servo.setTarget(i, int(state[i]*2000.0) + 6000)
+    avg_iters = 15
     accel = np.zeros(3)
     for i in range(avg_iters):
         data = sensor.get_accel_data()
-        accel[0] += data.x
-        accel[1] += data.y
-        accel[2] += data.z
+        accel[0] += data['x']
+        accel[1] += data['y']
+        accel[2] += data['z']
+        sleep(.05)
     return accel / float(avg_iters)
 
 sars = []
@@ -29,7 +29,23 @@ sars = []
 state = np.zeros(8)
 move_to(state)
 
+best_y = 0
+best_move = np.zeros(8)
+
+i = 0
 while True:
+    i += 1
+    if i % 5 == 0:
+        print("doing best move 4x")
+        for j in range(4):
+            move_to(state)
+            move_to(best_move)
+
+    move_to(state)
     state2 = np.random.random(8) * 2 - 1 # state between -1 and 1
-    accel = move_to(state)
+    accel = move_to(state2)
     print(accel)
+    if accel[1] > best_y:
+        print("new best ", accel[1])
+        best_y = accel[1]
+        best_move = state2.copy()
