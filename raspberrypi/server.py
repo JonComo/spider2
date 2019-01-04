14d61d8ac67cf5a39673fc12724359243582dc62
joncomo <jon@underground.net>
2019-01-04 00:02:26 -0800

    server change

--- raspberrypi/server.py
+++ raspberrypi/server.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
+import numpy as np
 import socket
 from socket import SOL_SOCKET, SO_REUSEADDR
+from time import sleep
 
 # servo
 # min 4000, mid 6000, max 8000
 import maestro
-from time import sleep
 
+# sensor
+from mpu6050 import mpu6050
+
+sensor = mpu6050(0x68)
 servo = maestro.Controller()
+
 for i in range(8):
     servo.setAccel(i, 30)
     servo.setTarget(i, 6000)
 
 TCP_IP = ''
 TCP_PORT = 5005
-BUFFER_SIZE = 200  # Normally 1024, but we want fast response
+BUFFER_SIZE = 200
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -38,9 +44,26 @@ while True:
                     # output received data
                     print ("Data: %s" % data)
                     d = data.split(",")
-                    for i in range(8):
-                        angle = float(d[i]) * 2000 + 6000 # angle between -1 and 1
-                        servo.setTarget(i, int(angle))
+                    if len(d) == 8:
+                        for i in range(8):
+                            angle = float(d[i]) * 2000 + 6000 # angle between -1 and 1
+                            servo.setTarget(i, int(angle))
+                    elif len(d) == 9:
+                        for i in range(8):
+                            angle = float(d[i]) * 2000 + 6000 # angle between -1 and 1
+                            servo.setTarget(i, int(angle))
+                        sensor_iters = int(d[8])
+                        accel = np.zeros(3)
+                        for j in range(sensor_iters):
+                            # collect sensor data
+                            data = sensor.get_accel_data()
+                            accel[0] += data['x']
+                            accel[1] += data['y']
+                            accel[2] += data['z']
+                            sleep(.05)
+                        s.send(accel.encode())
+
+
 
                 except:
                     print("error: unknown message")
